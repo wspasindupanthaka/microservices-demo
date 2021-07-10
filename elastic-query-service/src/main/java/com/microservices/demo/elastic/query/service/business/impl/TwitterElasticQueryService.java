3c3ef9f53dfe86d2075b90c73ee37c24469ed2da
wspasindupanthaka <wpasindupanthaka@gmail.com>
2021-07-10 23:25:53 +0530

    after lecture 49: Adding Hateoas capabilities

--- elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/business/impl/TwitterElasticQueryService.java
+++ elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/business/impl/TwitterElasticQueryService.java
@@ -4,6 +4,7 @@ import com.microservices.demo.elastic.model.index.impl.TwitterIndexModel;
 import com.microservices.demo.elastic.query.client.service.ElasticQueryClient;
 import com.microservices.demo.elastic.query.service.business.ElasticQueryService;
 import com.microservices.demo.elastic.query.service.model.ElasticQueryServiceResponseModel;
+import com.microservices.demo.elastic.query.service.model.assembler.ElasticQueryServiceResponseModelAssembler;
 import com.microservices.demo.elastic.query.service.transformer.ElasticToResponseModelTransformer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,31 +17,31 @@ public class TwitterElasticQueryService implements ElasticQueryService {
 
     private static final Logger LOG = LoggerFactory.getLogger(TwitterElasticQueryService.class);
 
-    private final ElasticToResponseModelTransformer elasticToResponseModelTransformer;
+    private final ElasticQueryServiceResponseModelAssembler elasticQueryServiceResponseModelAssembler;
 
     private final ElasticQueryClient<TwitterIndexModel> elasticQueryClient;
 
-    public TwitterElasticQueryService(ElasticToResponseModelTransformer elasticToResponseModelTransformer,
+    public TwitterElasticQueryService(ElasticQueryServiceResponseModelAssembler elasticQueryServiceResponseModelAssembler,
                                       ElasticQueryClient<TwitterIndexModel> elasticQueryClient) {
-        this.elasticToResponseModelTransformer = elasticToResponseModelTransformer;
+        this.elasticQueryServiceResponseModelAssembler = elasticQueryServiceResponseModelAssembler;
         this.elasticQueryClient = elasticQueryClient;
     }
 
     @Override
     public ElasticQueryServiceResponseModel getDocumentById(String id) {
         LOG.info("Querying Elasticsearch by Id {}", id);
-        return elasticToResponseModelTransformer.getResponseModel( elasticQueryClient.getIndexModelById(id));
+        return elasticQueryServiceResponseModelAssembler.toModel( elasticQueryClient.getIndexModelById(id));
     }
 
     @Override
     public List<ElasticQueryServiceResponseModel> getDocumentByText(String text) {
         LOG.info("Querying Elasticsearch by text {}", text);
-       return elasticToResponseModelTransformer.getReponseModels(elasticQueryClient.getIndexModelByText(text));
+       return elasticQueryServiceResponseModelAssembler.toModels(elasticQueryClient.getIndexModelByText(text));
     }
 
     @Override
     public List<ElasticQueryServiceResponseModel> getAllDocuments() {
         LOG.info("Querying all documents in Elasticsearch");
-        return elasticToResponseModelTransformer.getReponseModels(elasticQueryClient.getAllIndexModels());
+        return elasticQueryServiceResponseModelAssembler.toModels(elasticQueryClient.getAllIndexModels());
     }
 }
--- elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/model/ElasticQueryServiceResponseModel.java
+++ elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/model/ElasticQueryServiceResponseModel.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.hateoas.RepresentationModel;
 
 import java.time.LocalDateTime;
 
@@ -12,7 +13,7 @@ import java.time.LocalDateTime;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class ElasticQueryServiceResponseModel {
+public class ElasticQueryServiceResponseModel extends RepresentationModel<ElasticQueryServiceResponseModel> {
     private String id;
     private Long userId;
     private String text;
