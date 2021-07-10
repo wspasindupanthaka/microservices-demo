b67a79bfac8120b09edc9c5e1707090ec37e79a7
wspasindupanthaka <wpasindupanthaka@gmail.com>
2021-07-10 09:59:29 +0530

    after lecture 36: creating elastic-model module

---
+++ elastic/elastic-config/src/main/java/com/microservices/demo/elastic/config/ElasticsearchConfig.java
@@ -0,0 +1,43 @@
+package com.microservices.demo.elastic.config;
+
+import com.microservices.demo.config.ElasticConfigData;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Objects;
+
+@Configuration
+public class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
+
+    private final ElasticConfigData elasticConfigData;
+
+    public ElasticsearchConfig(ElasticConfigData configData) {
+        this.elasticConfigData = configData;
+    }
+
+    @Override
+    @Bean
+    public RestHighLevelClient elasticsearchClient() {
+        UriComponents serverUri = UriComponentsBuilder.fromHttpUrl(elasticConfigData.getConnectionUrl()).build();
+        return new RestHighLevelClient(
+                RestClient.builder(new HttpHost(
+                        Objects.requireNonNull(serverUri.getHost()),
+                        serverUri.getPort(),
+                        serverUri.getScheme()
+                )).setRequestConfigCallback(
+                        requestConfigBuilder ->
+                                requestConfigBuilder
+                                        .setConnectTimeout(elasticConfigData.getConnectionTimeout())
+                                        .setSocketTimeout(elasticConfigData.getSocketTimeout())
+
+                )
+        );
+    }
+
+}
