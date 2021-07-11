38e193522f948cef9bf727c5f922ffa6e4ca14ba
wspasindupanthaka <wpasindupanthaka@gmail.com>
2021-07-11 22:18:25 +0530

    after lecture 64: Creating reactive elastic web client

---
+++ reactive-elastic-query-web-client/src/main/java/com/microservices/demo/reactice/elastic/query/web/client/service/ElasticQueryWebClient.java
@@ -0,0 +1,10 @@
+package com.microservices.demo.reactice.elastic.query.web.client.service;
+
+import com.microservices.demo.elastic.query.web.client.common.model.ElasticQueryWebClientRequestModel;
+import com.microservices.demo.elastic.query.web.client.common.model.ElasticQueryWebClientResponseModel;
+import reactor.core.publisher.Flux;
+
+public interface ElasticQueryWebClient {
+
+    Flux<ElasticQueryWebClientResponseModel> getDataByText(ElasticQueryWebClientRequestModel request);
+}
