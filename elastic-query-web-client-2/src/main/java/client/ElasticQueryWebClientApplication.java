f44a6c3fcc875db668739c09f3df2e5d7b6a2995
wspasindupanthaka <wpasindupanthaka@gmail.com>
2021-07-13 11:50:45 +0530

    after lecture 71: Implementing SSO with Keycloak

---
+++ elastic-query-web-client-2/src/main/java/client/ElasticQueryWebClientApplication.java
@@ -0,0 +1,13 @@
+package client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.microservices.demo")
+public class ElasticQueryWebClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticQueryWebClientApplication.class, args);
+    }
+}
---
+++ elastic-query-web-client-2/src/main/java/client/service/ElasticQueryWebClient.java
@@ -0,0 +1,11 @@
+package client.service;
+
+
+import com.microservices.demo.elastic.query.web.client.common.model.ElasticQueryWebClientRequestModel;
+import com.microservices.demo.elastic.query.web.client.common.model.ElasticQueryWebClientResponseModel;
+
+import java.util.List;
+
+public interface ElasticQueryWebClient {
+    List<ElasticQueryWebClientResponseModel> getDataByText(ElasticQueryWebClientRequestModel requestModel);
+}
